20eac0336f7885f3b1bd32f6d35d6b66e9253924
Joel Fickson <Joelngozo@gmail.com>
2023-11-29 12:58:14 +0100

    chore: initial setup plus trying out hapi

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+'use strict';
+import  Hapi from '@hapi/hapi';
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+
+init();
