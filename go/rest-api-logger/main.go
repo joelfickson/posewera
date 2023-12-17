ac6f5ce69142a3c49e6f5cd685adaf94e1bf12e6
Joel Fickson <Joelngozo@gmail.com>
2023-12-17 01:24:42 +0100

    feat: create a REST api to create logs from an api

---
+++ go/rest-api-logger/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	}).GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
